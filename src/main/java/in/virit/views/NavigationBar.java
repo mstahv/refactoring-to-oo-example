eba0344ad12e5887a3bd23cd30d6fd033904378d
Matti Tahvonen <matti@vaadin.com>
2024-11-07 13:33:52 +0200

    Using better typed styling APIs and reordered visual tuning away from more relevant things

--- src/main/java/in/virit/views/NavigationBar.java
+++ src/main/java/in/virit/views/NavigationBar.java
@@ -11,6 +11,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.popover.Popover;
 import com.vaadin.flow.component.popover.PopoverPosition;
 import com.vaadin.flow.component.popover.PopoverVariant;
+import com.vaadin.flow.dom.Style;
 import com.vaadin.flow.router.Route;
 
 @Route
@@ -19,43 +20,47 @@ public class NavigationBar extends HorizontalLayout {
     private final Person person = new Person("John", "Doe", "https://randomuser.me/api/portraits/med/men/75.jpg");
 
     public NavigationBar() {
-        setSpacing(false);
-        getStyle().set("background", "var(--lumo-contrast-5pct)");
         add(new AvatarMenu(person));
+
+        setSpacing(false);
+        getStyle().setBackground("var(--lumo-contrast-5pct)");
     }
 
     private static class AvatarMenu extends Button {
         public AvatarMenu(Person person) {
             super(new PersonAvatar(person));
-            addThemeVariants(ButtonVariant.LUMO_ICON, ButtonVariant.LUMO_TERTIARY_INLINE);
-            getStyle().set("margin", "var(--lumo-space-s)");
-            getStyle().set("margin-inline-start", "auto");
-            getStyle().set("border-radius", "50%");
-            getIcon().getStyle().set("cursor", "pointer");
 
-            Popover popover = new Popover();
+            Popover popover = new Popover(
+                    new UserInfo(person),
+                    new MenuItems()
+            );
             popover.setTarget(this);
             popover.setModal(true);
+
             popover.setOverlayRole("menu");
             popover.setAriaLabel("User menu");
+
+            addThemeVariants(ButtonVariant.LUMO_ICON, ButtonVariant.LUMO_TERTIARY_INLINE);
+            getStyle().setMargin("var(--lumo-space-s)");
+            getStyle().setMarginInlineStart("auto");
+            getStyle().setBorderRadius("50%");
+            getIcon().getStyle().setCursor("pointer");
+
             popover.setPosition(PopoverPosition.BOTTOM_END);
             popover.addThemeVariants(PopoverVariant.LUMO_NO_PADDING);
-
-            popover.add(new UserInfo(person), new MenuItems());
 
         }
     }
 
     public static class MenuItems extends VerticalLayout {
         public MenuItems() {
-            setSpacing(false);
-            setPadding(false);
-            addClassName("userMenuLinks");
-
             addItem("User profile");
             addItem("Preferences");
             addItem("Sign out");
 
+            setSpacing(false);
+            setPadding(false);
+            addClassName("userMenuLinks");
         }
 
         public void addItem(String text) {
@@ -67,16 +72,14 @@ public class NavigationBar extends HorizontalLayout {
 
     public static class UserInfo extends HorizontalLayout {
         public UserInfo(Person person) {
-            addClassName("userMenuHeader");
-            setSpacing(false);
-
             Div fullName = new Div(person.fullName());
-            fullName.getStyle().set("font-weight", "bold");
             Div nickName = new Div(person.account());
-            nickName.addClassName("userMenuNickname");
-
             add(new LargePersonAvatar(person), new Div(fullName, nickName));
 
+            addClassName("userMenuHeader");
+            setSpacing(false);
+            fullName.getStyle().setFontWeight("bold");
+            nickName.addClassName("userMenuNickname");
         }
     }
 
@@ -84,7 +87,8 @@ public class NavigationBar extends HorizontalLayout {
         public PersonAvatar(Person person) {
             super(person.fullName());
             setImage(person.pictureUrl());
-            getStyle().set("display", "block");
+
+            getStyle().setDisplay(Style.Display.BLOCK);
             getElement().setAttribute("tabindex", "-1");
         }
     }
@@ -92,6 +96,7 @@ public class NavigationBar extends HorizontalLayout {
     private static class LargePersonAvatar extends PersonAvatar {
         public LargePersonAvatar(Person person) {
             super(person);
+
             addThemeVariants(AvatarVariant.LUMO_LARGE);
         }
     }
