75298a40f7b66dbcd58fc173109ff9cdcd4a2fd9
Matti Tahvonen <matti@vaadin.com>
2024-11-07 13:27:05 +0200

    refactoring

--- src/main/java/in/virit/views/NavigationBar.java
+++ src/main/java/in/virit/views/NavigationBar.java
@@ -1,6 +1,5 @@
 package in.virit.views;
 
-import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.avatar.Avatar;
 import com.vaadin.flow.component.avatar.AvatarVariant;
 import com.vaadin.flow.component.button.Button;
@@ -15,29 +14,36 @@ import com.vaadin.flow.component.popover.PopoverVariant;
 import com.vaadin.flow.router.Route;
 
 @Route
-public class PopoverUserMenu extends HorizontalLayout {
+public class NavigationBar extends HorizontalLayout {
 
-    private Person person = new Person("John", "Doe", "https://randomuser.me/api/portraits/med/men/75.jpg");
+    private final Person person = new Person("John", "Doe", "https://randomuser.me/api/portraits/med/men/75.jpg");
 
-    public PopoverUserMenu() {
+    public NavigationBar() {
         setSpacing(false);
         getStyle().set("background", "var(--lumo-contrast-5pct)");
+        add(new AvatarMenu(person));
+    }
 
-        Button button = new AvatarButton(person);
+    private static class AvatarMenu extends Button {
+        public AvatarMenu(Person person) {
+            super(new PersonAvatar(person));
+            addThemeVariants(ButtonVariant.LUMO_ICON, ButtonVariant.LUMO_TERTIARY_INLINE);
+            getStyle().set("margin", "var(--lumo-space-s)");
+            getStyle().set("margin-inline-start", "auto");
+            getStyle().set("border-radius", "50%");
+            getIcon().getStyle().set("cursor", "pointer");
 
-        Popover popover = new Popover();
-        popover.setModal(true);
-        popover.setOverlayRole("menu");
-        popover.setAriaLabel("User menu");
-        popover.setTarget(button);
-        popover.setPosition(PopoverPosition.BOTTOM_END);
-        popover.addThemeVariants(PopoverVariant.LUMO_NO_PADDING);
+            Popover popover = new Popover();
+            popover.setTarget(this);
+            popover.setModal(true);
+            popover.setOverlayRole("menu");
+            popover.setAriaLabel("User menu");
+            popover.setPosition(PopoverPosition.BOTTOM_END);
+            popover.addThemeVariants(PopoverVariant.LUMO_NO_PADDING);
 
-        HorizontalLayout userInfo = new UserInfo(person);
+            popover.add(new UserInfo(person), new MenuItems());
 
-        popover.add(userInfo, new MenuItems());
-
-        add(button, popover);
+        }
     }
 
     public static class MenuItems extends VerticalLayout {
@@ -69,26 +75,8 @@ public class PopoverUserMenu extends HorizontalLayout {
             Div nickName = new Div(person.account());
             nickName.addClassName("userMenuNickname");
 
-            add(
-                    new LargePersonAvatar(person),
-                    new Div(
-                        fullName,
-                        nickName
-                    )
-            );
+            add(new LargePersonAvatar(person), new Div(fullName, nickName));
 
-        }
-    }
-
-    private static class AvatarButton extends Button {
-        public AvatarButton(Person person) {
-            super(new PersonAvatar(person));
-            addThemeVariants(ButtonVariant.LUMO_ICON,
-                    ButtonVariant.LUMO_TERTIARY_INLINE);
-            getStyle().set("margin", "var(--lumo-space-s)");
-            getStyle().set("margin-inline-start", "auto");
-            getStyle().set("border-radius", "50%");
-            getIcon().getStyle().set("cursor", "pointer");
         }
     }
 
